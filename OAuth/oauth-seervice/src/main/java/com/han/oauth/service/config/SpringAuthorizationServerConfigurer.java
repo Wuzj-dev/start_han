ae29bfea40314e6197495cf7621b389645760795
Wzj <1500318810@qq.com>
2023-07-23 20:07:02 +0800

    oauth 密码模式

---
+++ OAuth/oauth-seervice/src/main/java/com/han/oauth/service/config/SpringAuthorizationServerConfigurer.java
@@ -0,0 +1,88 @@
+package com.han.oauth.service.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+import javax.annotation.Resource;
+
+/**
+ * @Author: Wuj
+ * @Description:
+ * @DateTime: 2023/7/23 16:44
+ **/
+@Configuration
+public class SpringAuthorizationServerConfigurer extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+
+//    @Resource
+//    PasswordEncoder passwordEncoder;
+
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+
+    @Override
+
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+
+                .withClient("spring")
+                .secret("{noop}secret")
+                .authorizedGrantTypes("refresh_token", "password", "client_credentials")
+                .scopes("webclient", "mobileclient");
+
+    }
+
+    /**
+     * 认证服务器的安全配置
+     *
+     * @param security
+     * @throws Exception
+     */
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security
+                //.realm(RESOURCE_ID)
+                // 开启/oauth/token_key验证端口认/oauth/check_token证权限访问
+                .tokenKeyAccess("isAuthenticated()")
+                //  开启/oauth/check_token验证端口认证权限访问
+//                .checkTokenAccess("isAuthenticated()")
+                // 允许接口 调用/oauth/check_token
+                    .checkTokenAccess("permitAll()")
+                //允许表单认证
+                .allowFormAuthenticationForClients();
+    }
+
+
+
+
+    @Override
+
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager).userDetailsService(userDetailsService);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- all-commom/src/main/java/com/han/allcommom/baseInfo/AbstractAppInfo.java
+++ all-commom/src/main/java/com/han/allcommom/baseInfo/AbstractAppInfo.java
@@ -14,5 +14,9 @@ public class AbstractAppInfo {
 
     public String applicationName;
 
+    public String secret;
+
+
+
 
 }
